0418981bfbead719679d547611e527ee0d94b38c
rbrthnrtz <robert.henritze@gmail.com>
2021-12-01 23:59:36 -0500

    solved 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+    let arrayWord = Array.from(str);
+    let reverseWord = [];
+    for (i = 0; i < str.length; i++) {
+        reverseWord.push(arrayWord.pop());
+    }
+    return reverseWord.join('');
 };
 
 // Do not edit below this line
