72155e0faddbebb4f8ce8a258d11cecdc58ad958
rbrthnrtz <robert.henritze@gmail.com>
2021-12-01 23:14:02 -0500

    solved 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        let newWord = [];
+        for (let i = 0; i < num; i++) {
+            newWord.push(word);
+        }
+        return newWord.join("");
+    }
 };
 
 // Do not edit below this line
