15e2afb7855b19d9f919dd339cd551b16bd7b73a
rbrthnrtz <robert.henritze@gmail.com>
2021-12-08 22:02:39 -0500

    solved 3 (review arrow funcs/ anon funcs)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  let oldArray = args[0];
 
+  let newArray = [];
+    
+
+  oldArray.forEach((element) => {
+    console.log(args.includes(element))
+    if (!args.includes(element)) {
+      newArray.push(element);
+    }
+  });
+
+  return newArray;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
