f038032051b5b0996c1060cd4afb272086ac4c24
rbrthnrtz <robert.henritze@gmail.com>
2021-12-12 13:37:35 -0500

    completed 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+// [°C] = ([°F] − 32) × 5⁄9
+let result = (num -32) * 5/9;
+return parseFloat(result.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+/// [°F] = [°C] × 9⁄5 + 32
+let result = num * 9/5 + 32
+return parseFloat(result.toFixed(1));
 };
+
+console.log(ftoc(32));
 
 // Do not edit below this line
 module.exports = {
